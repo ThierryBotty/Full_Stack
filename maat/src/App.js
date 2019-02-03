be54d04a8f9d1fb22afad7004fe7b15122a38ad6
Thierry <thierry.botty@helsinki.fi>
2019-02-04 00:44:00 +0200

    2.13 tehty

--- maat/src/App.js
+++ maat/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 const Country = ({ country }) => (
@@ -12,14 +12,31 @@ const Country = ({ country }) => (
     </ul>
 
     <img src={country.flag} width='200px' alt='flag' />
+
+
   </div>
 )
 
 const IndividualCountry = (props) => {
   return (
-    <div>{props.name} <button type="button" onclick={props.getCountry(props.name)}>Show</button>
+    <div>{props.name} <button type="button" onClick={props.click}>Show</button>
     </div>)
 }
+
+const Weather = (props) => (
+
+  <>
+    <div>
+    Temperature: {props.temp_c} Celcius
+    </div>
+    <div>
+    <img src={'https:' + (props.condition ? props.condition.icon : '//via.placeholder.com/150')} />
+    </div>
+    <div>
+    <b>Wind: </b>{props.wind_kph} kph, direction {props.wind_dir}
+  </div>
+</>
+)
 
 const Language = ( {language} ) => (
   <li> {language} </li>
@@ -44,13 +61,14 @@ const Countries = (props) => {
   return countries.map(country => <IndividualCountry
     key={country.name}
     name={country.name}
-    getCountry={props.getCountry()}/>)
+    click={() => props.getCountry(country.name)}/>)
 }
 
 const App = () => {
   const [ countries, setCountries ] = useState([])
   const [ filter, setFilter ] = useState('')
   const [ oldFilter, setOldFilter ] = useState('')
+  const [ weather, setWeather ] = useState({})
 
   const updateFilter = (event) => setFilter(event.target.value)
 
@@ -62,14 +80,22 @@ const App = () => {
     })
   }
 
+  if(countries.length == 1){
+    use
+  axios.get(`https://api.apixu.com/v1/current.json?key=278e909de30140c3950165341181010&q=${countries[0].capital}`).then(result => {
+    console.log(result.data.current)
+    setWeather(result.data.current)
+  })
+}
+
   if(filter !== '' && filter !== oldFilter){
-    console.log('effect')
     axios
       .get('https://restcountries.eu/rest/v2/name/' + filter)
       .then(response => {
         console.log('promise fulfilled')
         setCountries(response.data)
       })
+      setOldFilter(filter)
     }
 
   return(
@@ -78,8 +104,10 @@ const App = () => {
       <Countries
         countries = {countries}
         setCountries= {setCountries}
-        getCountry={getCountry()}
+        getCountry={getCountry}
       />
+      <Weather countries = {weather} />
+
     </div>
   )
 }
