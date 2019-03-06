27ff3ab325d6990e070ef19b4a2b8a1193a6619c
Thierry Botty <thierry.botty@helsinki.fi>
2019-03-06 23:06:31 +0200

    6.16

--- Viikko6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ Viikko6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,12 +1,3 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -17,12 +8,7 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
-
-const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
-
+const reducer = (state = [], action) => {
   switch(action.type) {
     case 'VOTE':
       return state.map(x => {
@@ -34,9 +20,15 @@ const reducer = (state = initialState, action) => {
     }).sort((a,b) => b.votes-a.votes)
   case 'ADD':
     return state.concat(asObject(action.data.content))
+  case 'INIT':
+    return action.data
   default:
     return state
   }
+}
+
+export const initA = anecdotes => {
+  return {type: 'INIT', data: { anecdotes }}
 }
 
 export const voteA = id => {
